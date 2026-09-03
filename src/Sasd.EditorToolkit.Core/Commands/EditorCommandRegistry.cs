d9be2447869d67b16daee8a45b5f64b3a10f46d2
Robin Goerlach (SASD-GmbH) <robin.goerlach@sasd.de>
2026-09-03 10:29:37 +0200

    feat(commands): register caret movement defaults

--- src/Sasd.EditorToolkit.Core/Commands/EditorCommandRegistry.cs
+++ src/Sasd.EditorToolkit.Core/Commands/EditorCommandRegistry.cs
@@ -22,6 +22,10 @@ public sealed class EditorCommandRegistry
         Register(new NewLineCommand());
         Register(new DeleteLeftCommand());
         Register(new DeleteRightCommand());
+        Register(new MoveCaretLeftCommand());
+        Register(new MoveCaretRightCommand());
+        Register(new MoveCaretUpCommand());
+        Register(new MoveCaretDownCommand());
         Register(new UndoCommand());
         Register(new RedoCommand());
     }
